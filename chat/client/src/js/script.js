4a940a9b534e9cdfcc4b0e58f401df1bfa27e4fe
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2024-08-06 11:56:41 -0300

    Iniciando o projeto de chat

---
+++ chat/client/src/js/script.js
@@ -0,0 +1,9 @@
+const login = {
+  area: document.querySelector('.login'),
+  form: document.querySelector('.login > .login__form'),
+  input: document.querySelector('.login > .login__input')
+};
+
+login.form.addEventListener("submit", () => {
+
+});
